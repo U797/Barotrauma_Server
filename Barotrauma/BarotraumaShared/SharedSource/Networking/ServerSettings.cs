70a2f03794d17e932dfba04d268a04a3842fd24b
Aaron <aaron.golder@gmx.de>
2023-07-14 17:21:45 +0200

    Add: Togglewiring Befehl hinzugefügt
    Togglewiring Befehl hinzugefügt

--- Barotrauma/BarotraumaShared/SharedSource/Networking/ServerSettings.cs
+++ Barotrauma/BarotraumaShared/SharedSource/Networking/ServerSettings.cs
@@ -658,7 +658,7 @@ namespace Barotrauma.Networking
             set;
         }
 
-        [Serialize(true, IsPropertySaveable.Yes)]
+        [Serialize(false, IsPropertySaveable.Yes)]
         public bool AllowRewiring
         {
             get;
